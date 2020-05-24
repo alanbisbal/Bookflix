266a64aa96f7e67f9f6332b1ed40bf9b0ff03693
anaclaramarco1@gmail.com <anaclaramarco1@gmail.com>
2020-05-24 03:22:30 -0300

    modifique agregarNovedad, falta arreglar el boton de agregar que esta feo

--- resources/views/agregarNovedad.blade.php
+++ resources/views/agregarNovedad.blade.php
@@ -1,54 +1,62 @@
 @extends('layouts.app')
 
+<link rel="stylesheet" href="css/estilos-agregarNovedad.css">
+
 @section('content')
-<div class="container">
+<div class="menu">
+  <div class="background-overlay">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card" style="height: 60%">
-                <div class="card-header"> Bienvenido {{auth()->user()->name}}
+            <div class="card" >
+                <div class="card-header"><h3>Nueva novedad:</h3>
                   @if (auth()->user()->es_admin)
-                     <p>(Administrador)</p>
                     <a class="nav-link" href="{{route('administracion')}}">Tareas administrativas</a>
                   @endif
-                 </div>
+                </div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                  @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                      {{ session('status') }}
+                    </div>
+                  @endif
+                  <form action="{{'novedadesCargados'}}" method="POST" enctype="multipart/form-data">
+                    {{csrf_field()}}
+                    <div class="contenedor-items">
+                      <div class="item1">
+                        @error('titulo')
+                        @enderror
+                        <div class="nombre">
+                          <label class="nombre" for="titulo"><h5>{{'Título:    '}}</h5></label>
                         </div>
-                    @endif
-
-
-              <form action="{{'novedadesCargados'}}" method="POST" enctype="multipart/form-data">
-                {{csrf_field()}}
-                @error('titulo')
-                @enderror
-                <label for="titulo">{{'Titulo:    '}}</label>
-                <input type="text" name="titulo" id="titulo" value="{{old('titulo')}}">
-                </br>
-                @if($errors->has('titulo'))
-                  El titulo no puede estar vacio
-                @endif
-              </br>
-                @error('desc')
-                @enderror
-
-                <label for="desc">{{'descripcion: '}}</label>
-                <input type="textarea" name="desc" id="desc" value="{{old('desc')}}">
-              </br>
-
-                @if($errors->has('desc'))
-                    La descripcion no puede estar vacia
-                @endif
-                </br>
-                <input type="submit" value="Agregar">
-              </form>
-
-
-
-
+                        <div class="texto">
+                          <input class="texto" type="text" name="titulo" id="titulo" value="{{old('titulo')}}">                    
+                        </div>
+                        @if($errors->has('titulo'))
+                          El titulo no puede estar vacio
+                        @endif             
+                      </div> 
+                      <div class="item2">
+                        @error('desc')
+                        @enderror
+                        <div class="nombre">
+                          <label for="desc"><h5>{{'Descripción: '}}</h5></label>
+                        </div>
+                        <div class="texto">
+                          <input class="labels" type="text" name="desc" id="desc" value="{{old('desc')}}">              
+                        </div>
+                        @if($errors->has('desc'))
+                            La descripcion no puede estar vacia
+                        @endif            
+                      </div>   
+                    </div> 
+                      <div class="item3">
+                        <input type="submit" value="Agregar novedad">
+                      </div>
+                  </form>
+                </div>
             </div>
         </div>
     </div>
+  </div>
 </div>
 @endsection
